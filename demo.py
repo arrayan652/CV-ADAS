f0b6e02e79c69323bf8d346a8244be237de870a1
daffadha <92097485+arrayan652@users.noreply.github.com>
2024-02-20 01:25:07 +0800

    demo.py

--- demo.py
+++ demo.py
@@ -16,12 +16,12 @@ LOGGER = Logger(None, logging.INFO, logging.INFO )
 
 video_path = "/content/drive/MyDrive/test5.mp4"
 lane_config = {
-	"model_path": "/content/drive/MyDrivetusimple_res18_fp16.trt",
+	"model_path": "/content/drive/MyDrive/tusimple_res18_fp16.trt",
 	"model_type" : LaneModelType.UFLDV2_TUSIMPLE
 }
 
 object_config = {
-	"model_path": '/content/drive/MyDriveyolov5n-coco_fp16.trt',
+	"model_path": '/content/drive/MyDrive/yolov5n-coco_fp16.trt',
 	"model_type" : ObjectModelType.YOLOV5,
 	"classes_path" : '/ObjectDetector/models/coco_label.txt',
 	"box_score" : 0.4,
