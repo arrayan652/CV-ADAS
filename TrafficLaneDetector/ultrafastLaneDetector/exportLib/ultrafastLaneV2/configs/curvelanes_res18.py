0bc2b234a3512033a3f57c300ca49d8703799e17
daffadha <92097485+arrayan652@users.noreply.github.com>
2024-02-23 15:45:29 +0800

    curvelanes_res18.py

--- TrafficLaneDetector/ultrafastLaneDetector/exportLib/ultrafastLaneV2/configs/curvelanes_res18.py
+++ TrafficLaneDetector/ultrafastLaneDetector/exportLib/ultrafastLaneV2/configs/curvelanes_res18.py
@@ -1,5 +1,5 @@
 dataset='CurveLanes'
-data_root= '' # Need to be modified before running
+data_root= '/content/Curvelanes' # Need to be modified before running
 epoch=50
 batch_size=8
 optimizer='SGD'
